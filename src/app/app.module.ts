952b0925eafad925a0a2e20b884acaabbbfb0ee8
hblandon <hlblandone@gmail.com>
2021-05-02 17:57:47 -0500

    inicio proyecto primates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comun/header/header.component';
+import { FooterComponent } from './comun/footer/footer.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductosComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
